fc9fda6750a5744b84fbf41900d854c79222208b
Nemish Mogra <mogranemish@gmail.com>
2026-02-12 11:55:51 +0530

    timeline in gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React from 'react'
+import { useRef } from 'react'
 
 const App = () => {
 
+  const boxRef = useRef(null)
+  const circleRef = useRef(null)
+  const tl=gsap.timeline()
+
   useGSAP(() => {
-    gsap.to(".box", {
-      x: 1000,
-      delay: 1,
-      duration: 2,
+    tl.to(boxRef.current, {
+      x: 1200,
       rotate: 360,
-      backgroundColor: "blue"
+      duration: 1.5,
+      delay:1,
+      borderRadius:"50%"
+    })
+
+    tl.to(circleRef.current, {
+      x: 1200,
+      rotate:360,
+      duration: 1.5,
+      borderRadius:"10px"
     })
   })
 
   return (
     <div>
-      <div className="box"></div>
+      <div className="box" ref={boxRef}></div>
+      <div className="circle" ref={circleRef}></div>
     </div>
   )
 }
