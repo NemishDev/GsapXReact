b8bc1dfb4a17979f0d65579c2297ce96482652c7
Nemish Mogra <mogranemish@gmail.com>
2026-02-12 12:07:18 +0530

    use of scope perform task on specific container

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,25 @@ import { useRef } from 'react'
 
 const App = () => {
 
-  const boxRef = useRef(null)
-  const circleRef = useRef(null)
-  const tl=gsap.timeline()
-
-  useGSAP(() => {
-    tl.to(boxRef.current, {
-      x: 1200,
-      rotate: 360,
-      duration: 1.5,
-      delay:1,
-      borderRadius:"50%"
-    })
-
-    tl.to(circleRef.current, {
-      x: 1200,
-      rotate:360,
-      duration: 1.5,
-      borderRadius:"10px"
-    })
+ useGSAP(function(){
+  gsap.to(".box",{
+    rotate:360,
+    duration:1.5,
+    delay:1
   })
+ }, {scope:'.container1'})
 
   return (
-    <div>
-      <div className="box" ref={boxRef}></div>
-      <div className="circle" ref={circleRef}></div>
-    </div>
+    <main>
+     <div className="container1">
+       <div className="box"></div>
+       <div className="circle"></div>
+     </div>
+     <div className="container2">
+       <div className="box"></div>
+       <div className="circle"></div>
+     </div>
+    </main>
   )
 }
 
